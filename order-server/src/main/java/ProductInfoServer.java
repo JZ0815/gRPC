74b617607740d8b90824251fa2e1684863c71837
Just Coding <jzhu@github.com>
2023-02-20 23:59:42 -0500

    change to maven

---
+++ order-server/src/main/java/ProductInfoServer.java
@@ -0,0 +1,25 @@
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import java.io.IOException;
+
+public class ProductInfoServer {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        int port = 50052;
+        Server server = ServerBuilder
+                .forPort(port)
+                .addService(new ProductInfoImpl())
+                .build();
+
+        server.start();
+        System.out.println("Server started");
+        System.out.println("Listing on port: " + port);
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("Recieved shutdown rquest");
+            server.shutdown();
+            System.out.println("Server stopped");
+        }));
+
+        server.awaitTermination();
+    }
+
+}
